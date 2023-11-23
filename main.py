e88f4689a9b1eb8843d76dbc54b40e8ff8c131c7
pat955 <patricijap8@gmail.com>
2023-11-23 12:45:26 +0100

    Ryddet en del av koden, la til mye dokumentasjon og la til et veldig enkelt error handling system.

--- main.py
+++ main.py
@@ -3,167 +3,226 @@ import json
 import os
 from datetime import datetime, date, timedelta
 
+# Global variables
 TODAY = date.today()
 TOMORROW = TODAY + timedelta(days=1)
 CLIENT_ID = 'weather-data/0.1 github.com/pat955'
 HEADERS = {'User-Agent': 'My User Agent 1.0', 'From': 'github.com/pat955/weather-data'}
 LOCFORECASTENDPOINT = 'https://api.met.no/weatherapi/locationforecast/2.0/compact'
 ELEVATION_ENDPOINT = 'https://api.open-meteo.com/v1/elevation'
-OSLO_COORDINATES = {'lat': '60', 'lon': '10', 'altitude': '148'}
 WD = 'weather_data.json'
 OPTION_FORMAT = {'1': 'mm', '2': '°C', '3': 'm/s'}
-
-
-
-# Se hva de har å si om last_modified i metno og hvordan jeg skal bruke det.
+NOMINATIM_ENDPOINT = 'https://nominatim.openstreetmap.org/search?format=geojson'
 
 def main():
-    user_input_city = input_loop()
+    """
+    First we get what city the user want to get information about and what they want to know.
+    Then we use that information to get coordinates, make sure we can use the cached data (if there is any).
+    The data gets redone if it has expired or if the previous coordinates arent the same (different cities)
+    Then we extract all the timeseries and remove todays
+    Lastly we call week_print that prints each days_average and break down.
+    """
+    user_input_city, coord_params = input_loop()
     options = options_loop()
     
-    coord_params = get_full_coordinates(user_input_city)
-    
     clean_data = get_clean_weather_json(coord_params)
-    all_timeseries = get_timeseries(clean_data)
-    all_timeseries = remove_todays_timeseries(all_timeseries)
-    
-    
-    current_date = TOMORROW
-    fullførte_kohorter = 0 
+    all_timeseries = (get_all_timeseries(clean_data))
+    relevant_timeseries = remove_todays_timeseries(all_timeseries)    
+
+    week_print(all_timeseries, user_input_city, options)
+
+
+def week_print(timeseries, user_input_city, options):
+    """
+    Print a weekly weather report for the specified city based on the provided time series data.
+
+    :param timeseries: Relevant time series data for a week. Big list
+    :param user_input_city: The name of the city for which the report is generated. Str
+    :param options: List of settings for the report. List
+    :return: None
+    """
+    current_date = TODAY
+    complete_cohorts = 0 
 
     for i in range(1, 7+1):
-        day_rep, fullførte_kohorter = day_report(all_timeseries, fullførte_kohorter, options)
-        dags_gjennomsnitt = {}
-        
-        print(f'Rapport {current_date.day}.{current_date.month}.{current_date.year} for {user_input_city.capitalize()}:')
-        
-        for hours_string, gjennomsnitter in day_rep.items():
-            string = f'kl.{hours_string} | '
-            for opt, gjennomsnitt in gjennomsnitter.items():
-                opt_format = OPTION_FORMAT[opt]
-                string += f'{round(float(gjennomsnitt),1)} {opt_format} | '
-            print(string)
-            #if total_gjennomsnitter == {}:
-             #   total_gjennomsnitter = gjennomsnitter
-            #total_gjennomsnitter = {k: total_gjennomsnitter.get(k, 0) + gjennomsnitter.get(k, 0) for k in total_gjennomsnitter}
-        
-        #print(f'Gjennomsnittstemperatur er {round(total_gjennomsnitter/4, 2)}°C')
-        print('======================================')
         current_date = current_date + timedelta(days=1)
-        
-def day_report(timeseries, fullførte_kohorter, options):
+        day_rep, complete_cohorts = day_report(timeseries, complete_cohorts, options)
+        day_average = {}
+        print(f'Rapport {current_date.day}.{current_date.month}.{current_date.year} for {user_input_city.capitalize()}:')
+
+        for hours_string, average in day_rep.items():
+            day_average = compound_day_average(day_average, hours_string, average)
+            result_string = f'kl.{hours_string} | '
+            for opt, average in average.items():
+                opt_format = OPTION_FORMAT[opt]
+                result_string += f'{round(float(average), 1)} {opt_format} | '
+            print(result_string)
+        day_print(day_average)
+
+
+def compound_day_average(day_average, hours_string, average): 
+    """
+    Compounds all averages into a day_average. Is used in a day loop.
+
+    :param day_average: dict of the previous day averages, first round is an empty dict.
+    :param hours_string: ex. 06-12. str
+    :param average: current average.
+    :return: day_average = {'1': x, '2': y}. Dict 
+    """
+    if day_average == {}:
+        day_average = average
+    day_average = {k: day_average.get(k, 0) + average.get(k, 0) for k in day_average}
+    return day_average
+
+
+def day_print(day_average):
+    """
+    Prints day averages in a pretty fashion.
+    :day_average: key = index from options, value = compounded averages of all day cohorts. dict
+    :return: None, prints
+    """
+    day_str = '| '
+    for i, average in day_average.items():
+        day_str += f'{round(average/4, 1)} '+ str(OPTION_FORMAT[i]) + ' | '
+    print('.'*39)
+    print(f'Snitt:   {day_str}')
+    print('=======================================')
+
+
+def option_results(next_timeserie, options):
+    """
+    Apply get function of options to averages
+    :param next_timeserie: singular next timeserie. dict
+    :return: Averages ex. {'1': 0.1, '3': 1.2} . dict
+    """
+    averages = {}
+    possible_answers = {'1': get_rain, '2': get_temperature, '3': get_wind_speed}
+    for opt, func in possible_answers.items():
+        if opt in options:
+            if opt not in averages:
+                averages[opt] = 0
+            averages[opt] += func(next_timeserie)
+    return averages
+
+
+def day_report(timeseries, complete_cohorts, options):
+    """
+    Possible answers are used to call function for each option
+    6 hours is one cohort, data has 36 hours not incl today. if 6 cohort havent been used,
+    averages are compounded and split into six until we reach 6 hours timeseries. 
+
+    :param timeseries: all relevant timeseries. list
+    :param complete_cohorts: current completed cohorts. int
+    :param options: user options. list
+    :return: day_dict, all 4 kohorts of the day split into timestamps and then into options. ex. {'00-06': {'2': 10, '3': 2}, ...}. dict 
+    :return: complete cohorts. int
+    """
     possible_answers = {'1': get_rain, '2': get_temperature, '3': get_wind_speed}
     day_dict = {}
     kohort_lengde = ('00-06', '06-12', '12-18', '18-24')           
-
-    if fullførte_kohorter < 6:
-
+    if complete_cohorts < 6:
         for quarter in range(4):
-            gjennomsnitt = {}
             for hour in range(6):
-                
                 next_timeserie = next_timeseries(timeseries)
-                for opt, func in possible_answers.items():
-
-                    if opt in options:
-                        if opt not in gjennomsnitt:
-                            gjennomsnitt[opt] = 0
-                        gjennomsnitt[opt] += func(next_timeserie)
+                average = option_results(next_timeserie, options)
                     
-                
-            day_dict[kohort_lengde[quarter]] = del_dict(gjennomsnitt)
+            day_dict[kohort_lengde[quarter]] = divide_dict(average)
             
-            fullførte_kohorter += 1
+            complete_cohorts += 1
     else:
         for quarter in range(4):
-            gjennomsnitt = {}
             next_timeserie = next_timeseries(timeseries)
-            for opt, func in possible_answers.items():
-                if opt in options:
-                    if opt not in gjennomsnitt:
-                        gjennomsnitt[opt] = 0
-                    gjennomsnitt[opt] += func(next_timeserie)
-            
-            day_dict[kohort_lengde[quarter]] = del_dict(gjennomsnitt)
-    return day_dict, fullførte_kohorter  
+            average = option_results(next_timeserie, options)
+            day_dict[kohort_lengde[quarter]] = divide_dict(average)
+    return day_dict, complete_cohorts  
 
-def del_dict(dictionary, split_amount=6):
+
+def divide_dict(dictionary, split_amount=6):
     for key, val in dictionary.items():
         dictionary[key] = val/6
     return dictionary
 
 
-def day_report1(timeseries, fullførte_kohorter):
-    day_dict = {}
-    kohort_lengde = ('00-06', '06-12', '12-18', '18-24', '00-06', '06-12')
-  
-    if fullførte_kohorter < 6:
-        for quarter in range(4):
-            gjennomsnitt_temp = 0
-            for hour in range(6):
-                next_timeserie = next_timeseries(timeseries)
-                gjennomsnitt_temp += get_temperature(next_timeserie)
-                print(get_rain(next_timeserie))
-                
-            day_dict[kohort_lengde[quarter]] = gjennomsnitt_temp/6
-            fullførte_kohorter += 1
-    else:
-        for quarter in range(4):
-            day_dict[kohort_lengde[quarter]] = get_temperature(next_timeseries(timeseries))
-    return day_dict, fullførte_kohorter
+def input_loop():
+    """
+    Asks user for input, 'q' to quit. It checks if the city exists and if it does, keeps the response to avoid calling it again.
+    :return: either reccursion because the city does not exist or return city name and full coordinates.
+    """
+    user_input = input('Skriv inn en by, pass på å skrive det riktig og at den eksisterer: ')
+    if user_input == 'q':
+        quit()
+    response = check_if_city_exists(user_input)
+
+    if response:
+        return user_input, get_full_coordinates(response)
+    print('Ikke eksisterende by, prøv igjen.\n')
+    return input_loop()
+
 
 def options_loop(lst=[]):
+    """
+    Get a valid option list, 'd' for done, 'a' for all and 'q' to exit script.
+    :param lst: defaults to empty list, can be used to update settings
+    :return: either reccursion if invalid answer or list of setting. list
+    """
     possible_answers = ['1', '2', '3']
     if lst == possible_answers:
         return list(set(lst))
-
     answer = input('Which of these would you like to enable?\n1. Rain\n2. Temperature\n3. Windspeed\nPress a number or "a" for all or "d" when done: ')
     if answer == 'q':
         exit()
-
     elif answer == 'a':
         return possible_answers
-
     elif answer == 'd':
         if lst == []:
             print('\nCan\'t be done with no options, press "a" for all.')
             return options_loop([])
         return list(set(lst))
-
-    if answer in possible_answers:
-        
+    elif answer in possible_answers:
         lst.append(answer)
         print(lst)
     return options_loop(lst)
 
-def log(*args, **kwargs):
-    pass
 
-def get_headers(coordinates):
-    lat, lon = coordinates['lat'], coordinates['lon']
-    headers_endpoint = f'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat={lat}&lon={lon}'
-    return requests.get(url=headers_endpoint, headers=HEADERS, auth=(CLIENT_ID, '')).headers
-   
-def get_full_coordinates(user_input):
-    nomi_endpoint = f'https://nominatim.openstreetmap.org/search?city={user_input}&format=geojson'
-    lat, lon = get_coordinates(make_request(nomi_endpoint))
-    return {'lat': round(float(lat), 2), 'lon': round(float(lon), 2), 'altitude': get_altitude({'latitude': lat, 'longitude': lon})}
+def get_full_coordinates(nominatim_response):
+    """
+    Get latitude, longitude and altitude. Altitude requires another API so it has to be separated.
+    :param nominatim_response: to avoid calling nminatim several times, if the city exists call get full coordinates on the response. str
+    :return: all coordinates needed for accurate locationforecast. ex. {'lat': 60, 'lon': 11, 'altitude': 151 }
+    """
+    lat, lon = get_lat_and_lon(nominatim_response)
+    lat, lon = round(float(lat)), round(float(lon))
+    return {'lat': lat, 'lon': lon, 'altitude': get_altitude({'latitude': lat, 'longitude': lon})}
     
+
 def get_altitude(coordinates):
     elevation_response = requests.get(ELEVATION_ENDPOINT, coordinates)
     elevation = (elevation_response.json())['elevation'][0]
     return int(elevation)
 
+
+def get_lat_and_lon(response):
+    coords = [float(num) for num in response.json()['features'][0]['geometry']['coordinates']]
+    return coords[1], coords[0]
+
+
 def get_clean_weather_json(coordinates):
+    """
+    Checks if file exists, is expired, empty and has the same coordinates as previous call. If so reuse data.
+    :param coordinates: coordinates used for requests and comparison. dict
+    :return: loaded json file. dict
+    """
     if os.path.exists(WD):
-        if os.path.getsize(WD) <= 3 or get_expiration() > datetime.now():
+        expired = get_expiration() > datetime.now()
+        if os.path.getsize(WD) <= 3 or expired:
+            print('expired: ', expired)
             os.remove(WD)
             make_new_weather_data_file(coordinates)
         else:
             with open(WD, "r") as file:
                 json_coords = json.load(file)['geometry']['coordinates']
                 if json_coords != [coordinates['lon'], coordinates['lat'], coordinates['altitude']]:
-                    print(f'coordinates did not match: {json_coords} /= [',coordinates['lon'], coordinates['lat'],coordinates['altitude'], ']')
+                    print('changed', json_coords, coordinates)
                     make_new_weather_data_file(coordinates)
     else:
         make_new_weather_data_file(coordinates)
@@ -171,103 +230,105 @@ def get_clean_weather_json(coordinates):
     with open("weather_data.json", "r") as opened_file:
         return json.load(opened_file)
 
+
 def make_new_weather_data_file(coordinates):
-    response = make_request(LOCFORECASTENDPOINT, coordinates)
+    """
+    Make new request, add headers and response data to json file. local caching and for speed.
+    :param coordinates: coordinates of dict to use as parameter for met.no
+    :return: None
+    """
+    response = make_request(LOCFORECASTENDPOINT, True, coordinates)
     headers = response.headers
     request_data = response.json()
     request_data['headers'] = dict(headers)
     with open("weather_data.json", "w") as file:
         json.dump(request_data, file)
 
-def input_loop():
-    user_input = input('Skriv inn en by, pass på å skrive det riktig og at den eksisterer: ')
-    if user_input == 'q':
-        exit()
-    elif user_input == 'c':
-        os.remove('weather_data.json')
-        return input_loop()
-    
-    if check_if_city_exists(user_input):
-        return user_input
-    print('Ikke eksisterende by, prøv igjen.\n')
-    return input_loop()
-
-def get_coordinates(response):
-    # round 2
-    data = response.json()
-    rounded_coords = [float(round(num)) for num in data['features'][0]['geometry']['coordinates']]
-    return rounded_coords[1], rounded_coords[0]
 
 def check_if_city_exists(city):
-    url = f'https://nominatim.openstreetmap.org/search?city={city}&format=geojson'
-    response = requests.get(url=url, headers=HEADERS, auth=(CLIENT_ID, ''))
-    return not len(response.text) <= 124
+    """
+    :param city: city name. str
+    :return: False if it doesn't exist or response if it does to avoid a second call. bool/response 
+    """
+    response = make_request(NOMINATIM_ENDPOINT, True, {'city': city})
+    if len(response.text) <= 124:
+        return False
+    return response
     
+
 def get_temperature(timeserie):
     return timeserie['data']['instant']['details']['air_temperature']
 
-def get_rain(timeserie):
-    try:
-        return timeserie['data']['next_1_hours']['details']['precipitation_amount']
-    except:
-        try:
-            return timeserie['data']['next_6_hours']['details']['precipitation_amount']
-        except:
-            try:
-                return timeserie['data']['next_12_hours']['details']['precipitation_amount']
-            except:
-                return 0.0
 
 def get_wind_speed(timeserie):
     return timeserie['data']['instant']['details']['wind_speed']
+
+
+def get_rain(timeserie):
+    """
+    Get predicted rain amount. No rain data in instant so next, 1 hour, 6 and twelve are those we can use.
+    :param timeserie: a timeserie. dict
+    :return: either an amount, or 0.0 if a value can't be found. float
+    """
+    time_intervals = ['next_1_hours', 'next_6_hours', 'next_12_hours']
+    for interval in time_intervals:
+        try: 
+            return timeserie['data'][interval]['details']['precipitation_amount']
+        except:
+            pass
+    return 0.0
     
-def get_timeseries(data):
+
+def make_request(endpoint, with_headers, params={}):
+    """
+    :param endpoint: endpoint, url. str
+    :param with_headers: bool 
+    :param params: parameters for request. dict 
+    :return: response. response
+    """
+    headers = HEADERS if with_headers else {}
+    auth = (CLIENT_ID, '') if with_headers else None
+    response = requests.get(endpoint, params=params, headers=headers, auth=auth)
+    error_handling(response.status_code)
+    return response
+
+
+def error_handling(status_code):
+    """
+    Simple error handling
+    :param status_code: status code from response. int
+    :return: True if ok or prints error message and recalls main.
+    """
+    if status_code == requests.codes.ok:
+        return True
+    else:
+        print('Error! Status Code:', status_code, '\nPlease retry. 5XX errors are out of our control.')
+        main()
+
+
+def get_all_timeseries(data):
     return data['properties']['timeseries']
+
 
 def remove_todays_timeseries(timeseries):
     while datetime.strptime(timeseries[0]['time'], '%Y-%m-%dT%H:%M:%SZ').date() == TODAY:
-        t = timeseries.pop(0)
+        timeseries.pop(0)
     return timeseries
+
 
 def next_timeseries(timeseries):
     return timeseries.pop(0)
+
 
 def get_expiration():
     with open(WD, "r") as file:
         expiration = json.load(file)['headers']['Expires']
     return datetime.strptime(expiration, '%a, %d %b %Y %H:%M:%S %Z')
 
-def get_last_modified():
-    with open(WD, "r") as file:
-        modified = json.load(file)['headers']['Last-Modified']
-    return datetime.strptime(modified, '%a, %d %b %Y %H:%M:%S %Z')
 
-def make_request_without_headers(endpoint, params=None):
-    if params == None:
-        return requests.get(endpoint)
-    return requests.get(endpoint, params)
+def quit():
+    print('Quitting...')
+    exit()
 
-def make_request(endpoint, params=None):
-    if params == None:
-        return requests.get(endpoint, headers=HEADERS, auth=(CLIENT_ID, ''))
-    return requests.get(endpoint, params, headers=HEADERS, auth=(CLIENT_ID,''))
-    
 
 main()
-# -A for authentication, -i to display body and header -I for only the latter. -s to not display progressbar(?) jsonpp to make it readable
-# curl -A "weather-data/0.1 github.com/pat955" -i -s 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=51.5&lon=0'|json_pp
-
-# Remove json_pp if using -I
-
-# curl -A "weather-data/0.1 github.com/pat955" -i -H 'If-Modified-Since: Tue, 16 Jun 2020 12:11:59 GMT' \
-#  -s 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=51.5&lon=0'
-"""
-def status_code_200(response):
-    if response.status_code == 200:
-        return True 
-    
-    print('Error! Returned status code %s' % request_data.status_code)
-    print('Message: %s' % json['error']['message'])
-    print('Reason: %s' % json['error']['reason'])
-    return False
-"""
